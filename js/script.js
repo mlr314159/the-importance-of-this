0a57c0f1ad92fdbd0fdd215ca32752fa5790d049
Mike <mlr314159@yahoo.co.uk>
2020-03-11 12:53:33 +0000

    last one

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-$(document).ready(function(){
-    $(".box").on("click", function(){
+$(document).ready(function() {
+    $(".box").on("click", function() {
+        /**
+        * when we click on an element that has a 'box' class, this event will be fired
+        */
         var classNames = $(this).attr("class").split(" ");
-        $("." + classNames[1]).css("background-color", "red");
+        var boxClass = classNames[0];
+        var className = classNames[1];
+        if($(this).css("background-color") == "rgb(255,0,0)") {
+            $("." + className).css("background-color", "#000");
+        } else {
+            $("." + boxClass).css("background-color", "#000"); 
+            $("." + className).css("background-color", "red");
+        }
+
     });
 });
